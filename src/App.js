d70dbcc139ced4211a31efa6890ae7b72ae5e27d
Raul.Laredo <Raul.Laredo@niceincontact.com>
2021-12-27 21:59:15 -0400

    Added folder structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import FeedbackItem from './components/FeedbackItem';
+import FeedbackList from './components/FeedbackList';
+import Headers from './components/Headers';
+import FeedbackData from './data/FeedbackData';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [feedback, setFeedback] = useState(FeedbackData)
+
+   const deleteFeedback = (id) => {
+      if(window.confirm('Are you sure you want to delete?')) {
+         setFeedback(feedback.filter((item)=> item.id !== id))
+      }
+      
+   }
+  
+  return ( 
+     <>
+      <Headers/>
+      <div className="container">
+         <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+      </div>
+     </>
+  )
 }
 
 export default App;
